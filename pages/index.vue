967a4d1aec8c89c64b5c3197e123c803b5bc30a4
Arifulhaque313 <asajib7654@gmail.com>
2022-06-19 00:18:45 +0600

    carosel part finished

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,72 @@
 <template>
 <div>
+    <vue-slick-carousel class="hero-slide" :arrows="false" :dots="true">
+        <div  class="px-28 py-8 ">
+          <div class="bg-green-100 rounded-2xl pt-8">
+              <div class="container">
+                <div class="flex items-center">
+                  <div class="w-1/2 mr-6">
+                      <p class="text-green-600 text-2xl font-normal">Save up 30% off</p>
+                      <h2 class="text-5xl font-bold text-gray-800 py-4">
+                        Bengal Vegetable Farm Organic 100%
+                      </h2>
+                      <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
+
+                      <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                  </div>
+                  
+                  <div class="w-1/2 flex justify-end">
+                      <img width="500" src="~/assets/images/hero.png" alt="">
+                  </div>
+              </div>
+              </div>
+          </div>
+      </div>   
+
+         <div  class="px-28 py-8">
+          <div class="bg-green-100 rounded-2xl pt-8">
+              <div class="container">
+                <div class="flex items-center">
+                  <div class="w-1/2 mr-6">
+                      <p class="text-green-600 text-2xl font-normal">Save up 40% off</p>
+                      <h2 class="text-5xl font-bold text-gray-800 py-4">
+                        Bengal Vegetable Farm Organic 100%
+                      </h2>
+                      <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
+
+                      <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                  </div>
+                  
+                  <div class="w-1/2 flex justify-end">
+                      <img width="500" src="~/assets/images/hero.png" alt="">
+                  </div>
+              </div>
+              </div>
+          </div>
+      </div> 
+
+         <div  class="px-28 py-8">
+          <div class="bg-green-100 rounded-2xl pt-8">
+              <div class="container">
+                <div class="flex items-center">
+                  <div class="w-1/2 mr-6">
+                      <p class="text-green-600 text-2xl font-normal">Save up 50% off</p>
+                      <h2 class="text-5xl font-bold text-gray-800 py-4">
+                        Bengal Vegetable Farm Organic 100%
+                      </h2>
+                      <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
+
+                      <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                  </div>
+                  
+                  <div class="w-1/2 flex justify-end">
+                      <img width="500" src="~/assets/images/hero.png" alt="">
+                  </div>
+              </div>
+              </div>
+          </div>
+      </div> 
+    </vue-slick-carousel>
     
 </div>
   
@@ -17,3 +84,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+   
+</style>
---
+++ plugins/vue-slick-carousel.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+
+Vue.component("VueSlickCarousel", require("vue-slick-carousel"));
