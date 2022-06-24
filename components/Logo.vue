9b0d6c2d57fa2a02a23d70b16f4394bf42f8ce7b
Arifulhaque313 <asajib7654@gmail.com>
2022-06-24 11:40:35 +0600

    search by category section complete

--- components/Logo.vue
+++ components/Logo.vue
@@ -2,7 +2,7 @@
     <div>
         <nuxt-link to="/" class="flex font-size-32 font-medium items-center">
                     <img class="mr-3" src="~/assets/images/logo.png" alt="logo">Online Shop
-                </nuxt-link>
+        </nuxt-link>
     </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,146 @@
 <template>
-<div>
-    <vue-slick-carousel class="hero-slide" :arrows="false" :dots="true">
-        <div  class="px-28 py-8 ">
-          <div class="bg-green-100 rounded-2xl pt-8">
-              <div class="container">
-                <div class="flex items-center">
-                  <div class="w-1/2 mr-6">
-                      <p class="text-green-600 text-2xl font-normal">Save up 30% off</p>
-                      <h2 class="text-5xl font-bold text-gray-800 py-4">
-                        Bengal Vegetable Farm Organic 100%
-                      </h2>
-                      <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
+    <div>
+        <vue-slick-carousel class="hero-slide" :arrows="false" :dots="true">
+            <div  class="px-48 py-8 ">
+              <div class="bg-green-100 rounded-2xl pt-8">
+                  <div class="container">
+                    <div class="flex items-center">
+                      <div class="w-1/2 mr-6">
+                          <p class="text-green-600 text-2xl font-normal">Save up 30% off</p>
+                          <h2 class="text-5xl font-bold text-gray-800 py-4">
+                            Bengal Vegetable Farm Organic 100%
+                          </h2>
+                          <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
 
-                      <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                          <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                      </div>
+                      
+                      <div class="w-1/2 flex justify-end">
+                          <img width="500" src="~/assets/images/hero.png" alt="">
+                      </div>
                   </div>
-                  
-                  <div class="w-1/2 flex justify-end">
-                      <img width="500" src="~/assets/images/hero.png" alt="">
                   </div>
               </div>
-              </div>
-          </div>
-      </div>   
+          </div>   
 
-         <div  class="px-28 py-8">
-          <div class="bg-green-100 rounded-2xl pt-8">
-              <div class="container">
-                <div class="flex items-center">
-                  <div class="w-1/2 mr-6">
-                      <p class="text-green-600 text-2xl font-normal">Save up 40% off</p>
-                      <h2 class="text-5xl font-bold text-gray-800 py-4">
-                        Bengal Vegetable Farm Organic 100%
-                      </h2>
-                      <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
+            <div  class="px-48 py-8">
+              <div class="bg-green-100 rounded-2xl pt-8">
+                  <div class="container">
+                    <div class="flex items-center">
+                      <div class="w-1/2 mr-6">
+                          <p class="text-green-600 text-2xl font-normal">Save up 40% off</p>
+                          <h2 class="text-5xl font-bold text-gray-800 py-4">
+                            Bengal Vegetable Farm Organic 100%
+                          </h2>
+                          <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
 
-                      <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                          <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                      </div>
+                      
+                      <div class="w-1/2 flex justify-end">
+                          <img width="500" src="~/assets/images/hero.png" alt="">
+                      </div>
                   </div>
-                  
-                  <div class="w-1/2 flex justify-end">
-                      <img width="500" src="~/assets/images/hero.png" alt="">
                   </div>
               </div>
-              </div>
-          </div>
-      </div> 
+          </div> 
 
-         <div  class="px-28 py-8">
-          <div class="bg-green-100 rounded-2xl pt-8">
-              <div class="container">
-                <div class="flex items-center">
-                  <div class="w-1/2 mr-6">
-                      <p class="text-green-600 text-2xl font-normal">Save up 50% off</p>
-                      <h2 class="text-5xl font-bold text-gray-800 py-4">
-                        Bengal Vegetable Farm Organic 100%
-                      </h2>
-                      <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
+            <div  class="px-48 py-8">
+              <div class="bg-green-100 rounded-2xl pt-8">
+                  <div class="container">
+                    <div class="flex items-center">
+                      <div class="w-1/2 mr-6">
+                          <p class="text-green-600 text-2xl font-normal">Save up 50% off</p>
+                          <h2 class="text-5xl font-bold text-gray-800 py-4">
+                            Bengal Vegetable Farm Organic 100%
+                          </h2>
+                          <p class="pb-10 pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sed obcaecati odio consequatur fuga?</p>
 
-                      <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                          <nuxt-link to="" class="rounded-full px-8 py-2 bg-green-500 text-white text-base">Shop now</nuxt-link>
+                      </div>
+                      
+                      <div class="w-1/2 flex justify-end">
+                          <img width="500" src="~/assets/images/hero.png" alt="">
+                      </div>
                   </div>
-                  
-                  <div class="w-1/2 flex justify-end">
-                      <img width="500" src="~/assets/images/hero.png" alt="">
                   </div>
               </div>
+          </div> 
+        </vue-slick-carousel>
+
+        <!-- search by category section start -->
+        <div class="container">
+            <h3 class="text-3xl mb-6 text-gray-800">Search by Category</h3>
+            <div class="flex items-center">
+                <div style="height:3px" class="w-36  bg-green-500"></div>
+                <div style="height:1px" class="w-full bg-gray-500"></div>
+            </div>
+
+            <!-- category carousel start -->
+         <vue-slick-carousel v-bind="categoryCarouselSettings"> 
+            <div class="bg-gray-200 p-4 m-4">
+              <div class="h-32 flex justify-center items-center">
+                  <img src="~/assets/images/caro1.png" alt="">
               </div>
-          </div>
-      </div> 
-    </vue-slick-carousel>
-    
-</div>
-  
-  
+              <div class="text-center">
+                  <h3>Groceries</h3>
+              </div>
+            </div>
+
+            <div class="bg-gray-200 p-4 m-4">
+              <div class="h-32 flex justify-center items-center">
+                  <img src="~/assets/images/caro2.png" alt="">
+              </div>
+              <div class="text-center">
+                  <h3>Groceries</h3>
+              </div>
+            </div>
+
+            <div class="bg-gray-200 p-4 m-4">
+              <div class="h-32 flex justify-center items-center">
+                  <img src="~/assets/images/caro3.png" alt="">
+              </div>
+              <div class="text-center">
+                  <h3>Groceries</h3>
+              </div>
+            </div>
+
+            <div class="bg-gray-200 p-4 m-4">
+              <div class="h-32 flex justify-center items-center">
+                  <img src="~/assets/images/caro4.png" alt="">
+              </div>
+              <div class="text-center">
+                  <h3>Groceries</h3>
+              </div>
+            </div>
+
+            <div class="bg-gray-200 p-4 m-4">
+              <div class="h-32 flex justify-center items-center">
+                  <img src="~/assets/images/caro5.png" alt="">
+              </div>
+              <div class="text-center">
+                  <h3>Groceries</h3>
+              </div>
+            </div>
+
+            <div class="bg-gray-200 p-4 m-4">
+              <div class="h-32 flex justify-center items-center">
+                  <img src="~/assets/images/caro6.png" alt="">
+              </div>
+              <div class="text-center">
+                  <h3>Groceries</h3>
+              </div>
+            </div>
+         </vue-slick-carousel>
+
+        <!-- category carousel end  -->
+        </div>
+        <!-- search by category section end  -->
+
+        
+
+        
+    </div> 
 </template>
 
 <script>
@@ -81,6 +151,14 @@ export default {
   name: 'IndexPage',
   components:{
     Header,Footer,Logo
+  },
+  data(){
+    return{
+      categoryCarouselSettings:{
+        'slidesToShow':5,
+        
+      }
+    }
   }
 }
 </script>
